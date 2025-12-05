cc970e46e19f422091cd267fcc4a15dca7a0a0f3
Andres Ortega-Guerrero <34098967+AndresOrtegaGuerrero@users.noreply.github.com>
2025-12-05 11:12:21 +0100

    Update pyproject and version (#10)

--- src/meqpy/__init__.py
+++ src/meqpy/__init__.py
@@ -2,6 +2,8 @@
 meqpy: A modular Python framework for defining systems and solving master equations.
 """
 
+__version__ = "0.0.1"
+
 from .system.system import System
 from .system.state import State
 from .master_equation.equilibrium import solve_equilibrium
