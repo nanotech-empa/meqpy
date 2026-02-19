e8700bb1b3fca033c2ff833d22b7b508b262f3a0
NilsKrane <143397408+NilsKrane@users.noreply.github.com>
2026-02-19 14:58:37 +0100

    Removed transition.py and updated init files

--- src/meqpy/__init__.py
+++ src/meqpy/__init__.py
@@ -6,6 +6,7 @@ __version__ = "0.0.1"
 
 from .system.system import System
 from .system.state import State
+from .io.cube import Cube, TBCube
 from .master_equation.equilibrium import (
     solve_equilibrium,
     solve_equilibrium_nd,
@@ -16,7 +17,8 @@ from .master_equation.measurement import measurement
 __all__ = [
     "System",
     "State",
-    "Transition",
+    "Cube",
+    "TBCube",
     "solve_equilibrium",
     "solve_equilibrium_nd",
     "fill_diagonal",
