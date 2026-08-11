a87f45c7db39575ff406bb240e461db76804fb83
NilsKrane <143397408+NilsKrane@users.noreply.github.com>
2026-08-11 13:41:51 +0200

    Streamlined internal code and removed type checking

--- tests/test_opticaltransition.py
+++ tests/test_opticaltransition.py
@@ -38,12 +38,6 @@ class TestOpticalTransitionInit:
             OpticalTransition("not_a_real_path.cube")
 
 
-class TestVoxelSize:
-    def test_voxel_size_cartesian(self, cube_path):
-        optical = OpticalTransition(cube_path("cartesian"))
-        assert np.isclose(optical.voxel_size, 0.125, atol=1e-5)
-
-
 class TestKernelMesh:
     def test_shape_no_pad(self, cube_path):
         optical = OpticalTransition(cube_path("cartesian"))
@@ -73,31 +67,16 @@ class TestKernelMesh:
 
         assert np.allclose(kernel_mesh[2][0, 0, :], optical.z, atol=1e-6)
 
-    def test_negative_pad_raises(self, cube_path):
-        optical = OpticalTransition(cube_path("cartesian"))
-        with pytest.raises(ValueError) as e_info:
-            optical._kernel_mesh(pad=-1)
-        assert str(e_info.value) == "pad must be >= 0, got -1"
-
-    def test_float_pad_raises(self, cube_path):
-        optical = OpticalTransition(cube_path("cartesian"))
-        with pytest.raises(TypeError) as e_info:
-            optical._kernel_mesh(pad=0.5)
-        assert str(e_info.value) == "pad must be int, got float"
-
 
 class TestCoulombPotential:
     def test_potential_is_zero_if_too_close(self):
         mesh_x = np.array([0.5])
         mesh_y = np.array([0.0])
         mesh_z = np.array([0.0])
+        mesh = [mesh_x, mesh_y, mesh_z]
 
         potential = OpticalTransition._coulomb_potential(
-            mesh_x,
-            mesh_y,
-            mesh_z,
-            x_pointcharge=0.0,
-            y_pointcharge=0.0,
+            mesh,
             z_pointcharge=0.0,
         )
 
@@ -107,80 +86,42 @@ class TestCoulombPotential:
         mesh_x = np.array([1.0, 2.0, 4.0])
         mesh_y = np.zeros(3)
         mesh_z = np.zeros(3)
+        mesh = [mesh_x, mesh_y, mesh_z]
 
         potential = OpticalTransition._coulomb_potential(
-            mesh_x,
-            mesh_y,
-            mesh_z,
-            x_pointcharge=0.0,
-            y_pointcharge=0.0,
+            mesh,
             z_pointcharge=0.0,
         )
 
         # Coulomb potential must fall off with increasing distance.
         assert potential[0] > potential[1] > potential[2] > 0
 
-    def test_mismatched_shapes_raise(self):
-        with pytest.raises(ValueError) as e_info:
-            OpticalTransition._coulomb_potential(
-                np.zeros((2, 2)), np.zeros((3, 3)), np.zeros((2, 2)), 0.0, 0.0, 0.0
-            )
-        assert str(e_info.value) == (
-            "mesh_x, mesh_y and mesh_z must be of same shape, but got "
-            "arrays with shape (2, 2),  (3, 3), and  (2, 2)"
-        )
-
-    def test_mesh_wrong_type_raises(self):
-        with pytest.raises(TypeError) as e_info:
-            OpticalTransition._coulomb_potential(
-                [0.0], np.array([0.0]), np.array([0.0]), 0.0, 0.0, 0.0
-            )
-        assert str(e_info.value) == "mesh_x must be ndarray, but got list"
-
-    def test_pointcharge_wrong_type_raises(self):
-        mesh = np.array([0.0])
-        with pytest.raises(TypeError) as e_info:
-            OpticalTransition._coulomb_potential(mesh, mesh, mesh, "0", 0.0, 0.0)
-        assert str(e_info.value) == "x_pointcharge must be Real, but got str"
-
 
 class TestTwoPointCharges:
     def test_zero_when_charge_on_mirror_plane(self, meshgrid):
-        mesh_x, mesh_y, mesh_z = meshgrid()
         potential = OpticalTransition._two_point_charges(
-            mesh_x, mesh_y, mesh_z, mirror_plane=2.0, z_pointcharge=2.0
+            meshgrid(), mirror_plane=2.0, z_pointcharge=2.0
         )
 
         assert np.allclose(potential, 0.0, atol=1e-8)
 
     def test_zero_on_mirror_plane_itself(self, meshgrid):
         mirror_plane = 2.0
-        mesh_x, mesh_y, mesh_z = meshgrid(z_value=mirror_plane)
         potential = OpticalTransition._two_point_charges(
-            mesh_x, mesh_y, mesh_z, mirror_plane=mirror_plane, z_pointcharge=7.0
+            meshgrid(z_value=mirror_plane), mirror_plane=mirror_plane, z_pointcharge=7.0
         )
 
         assert np.allclose(potential, 0.0, atol=1e-8)
 
     def test_antisymmetric_about_mirror_plane(self, meshgrid):
-        mesh_x, mesh_y, mesh_z = meshgrid(z_value=2.0)
-
         above = OpticalTransition._two_point_charges(
-            mesh_x, mesh_y, mesh_z, mirror_plane=0.0, z_pointcharge=5.0
+            meshgrid(z_value=2.0), mirror_plane=0.0, z_pointcharge=5.0
         )
         below = OpticalTransition._two_point_charges(
-            mesh_x, mesh_y, -mesh_z, mirror_plane=0.0, z_pointcharge=5.0
+            meshgrid(z_value=-2.0), mirror_plane=0.0, z_pointcharge=5.0
         )
 
         assert np.allclose(above, -below, atol=1e-8)
-
-    def test_mirror_plane_wrong_type_raises(self):
-        mesh = np.array([0.0])
-        with pytest.raises(TypeError) as e_info:
-            OpticalTransition._two_point_charges(
-                mesh, mesh, mesh, mirror_plane="0", z_pointcharge=1.0
-            )
-        assert str(e_info.value) == "mirror_plane must be Real, but got str"
 
 
 class TestPlasmonCoupling:
